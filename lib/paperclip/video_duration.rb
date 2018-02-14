4123c54287220d75c4a8cf32ff410fcd282ca27f
Christian Jeffries <christian@proxima.io>
2018-02-14 16:04:51 +1100

    Adding in ability for Paperclip to validate attached video duration

--- lib/paperclip/video_duration.rb
+++ lib/paperclip/video_duration.rb
@@ -1,16 +1,15 @@
 require 'ffprober'
 
 module Paperclip
-
-  # Defines the geometry of a video
-  class VideoGeometry
+  # Get duration of a video
+  class VideoDuration
     class << self
-      def from_file_path(file)
-        probe = Ffprober::Parser.from_file(file)
+      def from_file_path(path)
+        probe = Ffprober::Parser.from_file(path)
         return nil if probe.blank?
         video_stream = probe.video_streams.try(:first)
         return nil if video_stream.blank?
-        video_stream
+        video_stream.try(:duration).try(:to_f)
       end
     end
   end
